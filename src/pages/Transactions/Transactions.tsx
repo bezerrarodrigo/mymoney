a48db2ca8061769d473bd6ac430fdec1c9be6de0
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-12-13 17:25:59 -0300

    feat: summary component

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,7 +1,11 @@
 import {Header} from "../../components/Header/Header.tsx";
+import {Summary} from "../../components/Summary/Summary.tsx";
 
 export const Transactions = () => {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Summary/>
+    </>
   );
 };
