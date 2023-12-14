60013ea1d9c9f641cf522b3c61fbd74ad0c4b162
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-12-14 13:23:59 -0300

    feat: transactions table

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,11 +1,38 @@
-import {Header} from "../../components/Header/Header.tsx";
-import {Summary} from "../../components/Summary/Summary.tsx";
+import { Header } from "../../components/Header/Header.tsx";
+import { Summary } from "../../components/Summary/Summary.tsx";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./style.ts";
 
 export const Transactions = () => {
   return (
     <>
-      <Header/>
-      <Summary/>
+      <Header />
+      <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighLight variant="outcome"> - R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   );
 };
