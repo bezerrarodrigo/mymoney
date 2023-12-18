9649357df717f676cb759565ef547f918db619d4
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-12-18 11:42:35 -0300

    feat: added accessible modal component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,28 @@
-import {HeaderContainer, HeaderContent, NewTransactionButton} from "./style.ts";
-import logoImg from '../../assets/logo.svg';
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionButton,
+} from "./style.ts";
+import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg} alt=""/>
-        <NewTransactionButton>New transaction</NewTransactionButton>
+        <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New transaction</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>New Transaction</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
