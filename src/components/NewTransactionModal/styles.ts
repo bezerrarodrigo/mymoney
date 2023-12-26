e75ce467ff320e53071807204c320653982f6d37
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-12-26 14:40:51 -0300

    feat: added hook form and zod schema

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -50,7 +50,12 @@ export const Content = styled(Dialog.Content)`
       padding: 0 1.25rem;
       margin-top: 1.5rem;
 
-      &:hover {
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background: ${(props) => props.theme["green-700"]};
         transition: background-color 0.2s;
       }
